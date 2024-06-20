fd2257d5b2d38cbc526060b6216f1e70e202f756
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2024-06-20 22:28:37 +0400

    our-story part-2

--- src/pages/OurStory.jsx
+++ src/pages/OurStory.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import img1 from '../images/Image-7-scaled-600x346.webp'
 import img2 from '../images/Image-3-700x590.webp'
+import img3 from '../images/Image-12-scaled-600x400.webp'
 import AOS from 'aos'
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import newsData from '../data/news';
+import logo from '../images/logo-small.svg';
 
 const OurStory = () => {
 
@@ -83,22 +86,22 @@ const OurStory = () => {
                   <div><span data-aos-delay="300" data-aos="fade-up">every corner.</span></div>
                 </div>
               </h5>
-              <p data-aos="fade-up">
-                <span>Frustrated with the monotonous options available in the market,
+              <p>
+                <span data-aos="fade-up">Frustrated with the monotonous options available in the market,
                   I dared to dream up an e-bike that transformed from a humble idea into
                   an extraordinary reality.</span>
 
-                <span>Each SentiMental bike is meticulously handcrafted, ensuring that our
+                <span data-aos="fade-up">Each SentiMental bike is meticulously handcrafted, ensuring that our
                   passion for detail and dedication to quality shines through. With a subtle
                   nod to the golden era of biking, our timeless designs pay homage to the
                   past while embracing the future.</span>
 
-                <span>Our e-bikes are more than just a mode of transportation; they’re an
+                <span data-aos="fade-up">Our e-bikes are more than just a mode of transportation; they’re an
                   invitation to experience the world in a whole new way.</span>
               </p>
             </div>
             <div className="img-container">
-              <img src={img2} alt="" />
+              <img data-aos="zoom-out" src={img2} alt="" />
             </div>
           </div>
           <div className="result">
@@ -119,10 +122,102 @@ const OurStory = () => {
                 </div>
               </h1>
               <p>
-                <span data-aos="fade-up">Together, we’ll create memories that will last a lifetime.</span>
+                <span data-aos-delay="300" data-aos="fade-up">Together, we’ll create memories that will last a lifetime.</span>
               </p>
             </div>
           </div>
+        </div>
+      </section>
+      <div className="box-container">
+        <div className='white-box'></div>
+        <div className='green-box'></div>
+      </div>
+      <section id='sentimental-news-story'>
+        <div className="container">
+          <div className="h1-container">
+            <h1 data-aos="fade-up" className='hero-h1'>SentiMental in the Spotlight</h1>
+          </div>
+          <div style={{ overflow: "hidden" }}>
+            {newsData.map(item => (
+              <div data-aos-delay={`${(item.id - 1) * 100}`} data-aos="fade-up" key={item.id} className="card-container">
+                <div className='first-container'>
+                  <span className='desktop-news'>NEWS</span>
+                  <div className="img-container">
+                    <img src={`/src/images/${item.img}`} alt="" />
+                  </div>
+                  <span className='mobile-news'>NEWS</span>
+                </div>
+                <div className="second-container">
+                  <h1>{item.title}</h1>
+                  <div className="date-author">
+                    <i className="fa-regular fa-calendar"></i>
+                    <div className="date">
+                      <span>{item.date}</span>
+                    </div>
+                    <span className='seperator'>|</span>
+                    <div className="author">
+                      <span>{item.author}</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="button-container">
+            <button>ALL MEDIA</button>
+          </div>
+        </div>
+      </section>
+      <section id='feeling-sentimental-story'>
+        <div className="container">
+          <div className="h1-container">
+            <h1 data-aos-delay="0" data-aos="fade-up"><div>Feeling Sentimental? We</div> got you.</h1>
+          </div>
+          <div className="p-container">
+            <p data-aos-delay="100" data-aos="fade-up">Falling in love is easy. Staying in love takes work.
+              This is why we used exceptional materials and long-range
+              batteries wrapped in timeless design. So that your love for
+              SentiMental bikes lasts today, tomorrow, and beyond.</p>
+          </div>
+          <div className="logo-container">
+            <div data-aos-delay="200" data-aos="fade-up" className='logo-name'>
+              <img src={logo} alt="" />
+              <div>
+                <span>Dule</span>
+                <span>Sentimental Bikes</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id='ride-the-wave'>
+        <div className="container">
+          <div className="text-container">
+            <h1>
+              <div className="mobile">
+                <div>
+                  <span data-aos-delay="0" data-aos="fade-up">Go ahead. Ride the</span>
+                </div>
+                <div>
+                  <span data-aos-delay="100" data-aos="fade-up">wave. For SentiMental</span>
+                </div>
+                <div>
+                  <span data-aos-delay="200" data-aos="fade-up">reasons.</span>
+                </div>
+              </div>
+              <div className="desktop">
+                <div>
+                  <span data-aos-delay="0" data-aos="fade-up">Go ahead. Ride the wave.</span>
+                </div>
+                <div>
+                  <span data-aos-delay="100" data-aos="fade-up">For SentiMental reasons.</span>
+                </div>
+              </div>
+            </h1>
+          </div>
+        </div>
+        <div className="img-container">
+          <img src={img3} alt="" />
         </div>
       </section>
     </>
