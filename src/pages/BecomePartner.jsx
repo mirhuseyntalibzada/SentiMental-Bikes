53d191da81edde8e0bd275781f72ad647c5944fb
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2024-06-22 14:47:06 +0400

    become-a-partner

--- src/pages/BecomePartner.jsx
+++ src/pages/BecomePartner.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Form from '../components/Form'
+import BecomePartnerComp from '../components/BecomePartnerComp'
 
 const BecomePartner = () => {
   return (
-    <div>BecomePartner</div>
+    <>
+    <Form data={{
+      h1: "Become a partner",
+      p: `Feeling the SentiMental vibe? Great! We are always looking for like-minded partners who share our values. Whether you're a bike shop, a cycling club, a company, or an individual with a burning passion for e-bikes, there's a place for you in our #itsrideoclock community.`
+    }} />
+    <BecomePartnerComp style={{ backgroundColor: "rgb(205, 205, 199)" }} />
+    </>
   )
 }
 
