a1c82b154fd065ad4c0902e863ae4ca689465524
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2024-07-13 20:33:22 +0400

    cart section

---
+++ src/components/BillingAddress.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const BillingAddress = () => {
+  return (
+    <>
+    <h1>BIlling details</h1>
+    <h6>FIRST NAME*</h6>
+    <input type="text" />
+    <h6>LAST NAME*</h6>
+    <input type="text" />
+    </>
+  )
+}
+
+export default BillingAddress
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
+import supabase from '../config/connect'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useCookies } from 'react-cookie'
+import BillingAddress from '../components/BillingAddress'
 
 const Cart = () => {
-  const cartData = useSelector((state) => state.cart) 
+  const [items, setItems] = useState({})
+  const [amount, setAmount] = useState(0)
+  const [quantity, setQuantity] = useState(0)
+  const [cookie] = useCookies(['cookie-user'])
+
+  const handleChange = (e) => {
+    const newValue = parseInt(e.target.value, 10);
+    if (!isNaN(newValue)) {
+      setValue(newValue);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await supabase.from('users').select()
+      const user = data.find(({ token }) => token === cookie['cookie-user'])
+      setItems(user.cart.cart);
+      setAmount(user.cart.cartTotalAmount);
+      setQuantity(user.cart.cartTotalQuantity);
+    }
+    fetchData()
+  }, [])
+
   return (
     <>
-      {cartData.cart.map((item, i) => (
-        <h1 key={i}>{item.handle_color}</h1>
-      ))}
       <section id='cart'>
         <div className="container">
           <div className="text-container">
-            <h1>Cart</h1>
+            {items.length === undefined ? <h1>Cart</h1> : <h1>Checkout</h1>}
           </div>
         </div>
       </section>
@@ -22,19 +45,94 @@ const Cart = () => {
           <div className="white-box"></div>
         </div>
         <div className="container">
-          {cartData.cart.length === 0 ?
-            <>
-              <div className="message">
-                <span>Your cart is currently empty.</span>
+          <div>
+            {items.length === undefined ?
+              <>
+                <div className="message">
+                  <span>Your cart is currently empty.</span>
+                </div>
+                <button className='return-to-shop'>
+                  <NavLink to={"/configure-a-bike"} onClick={() => { window.scrollTo(0, 0) }}>
+                    RETURN TO SHOP
+                  </NavLink>
+                </button>
+              </>
+              : <div>{items.length !== undefined ?
+                items.map((item, i) => (
+                  <>
+                    <div className='product-card' key={i}>
+                      <div className='img-text-container'>
+                        <div className='img-container'>
+                          <img src={`/src/images/bicycles/handlebars/${item.handle_color.split(' ').join('-')}/bicycle-color/${item.bicycle_color.split(' ').join('-')}.png`} alt="" />
+                        </div>
+                        <div className='text-container'>
+                          <h5>Configure a bike</h5>
+                          <div className='category-name'>
+                            <h6>Frame Type</h6>
+                            <p>Noogat</p>
+                          </div>
+                          <div className='category-name'>
+                            <h6>Tyres</h6>
+                            <p>Standart</p>
+                          </div>
+                          <div className='category-name'>
+                            <h6>Bicycle color</h6>
+                            <p>{item.bicycle_color}</p>
+                          </div>
+                          <div className='category-name'>
+                            <h6>Handle color</h6>
+                            <p>{item.handle_color}</p>
+                          </div>
+                          <div className='price-quantity-container'>
+                            <div>
+                              <h5>€{item.price * item.quantity}.00</h5>
+                            </div>
+                            <div className='quantity'>
+                              <a href="#!" className='minus'>-</a>
+                              <input value={item.quantity} type="number" onChange={handleChange} />
+                              <a href="#!" className='plus'>+</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="delete">
+                        <i className="fa-solid fa-x"></i>
+                      </div>
+                    </div>
+                  </>
+                )) : ''}
+                <div className="amount-container">
+                  <div className='amount-info'>
+                    <h1>Subtotal:</h1>
+                    <span>€{amount}.00</span>
+                  </div>
+                  <div className='amount-info'>
+                    <h1>Shipping:</h1>
+                    <span>€{50 * quantity}.00</span>
+                  </div>
+                  <div className='amount-info'>
+                    <h1>Total:</h1>
+                    <span>€{amount + (50 * quantity)}.00</span>
+                  </div>
+                  <div className='coupon-container'>
+                    <input placeholder='Coupon code' type="text" />
+                    <button>APPLY COUPON</button>
+                  </div>
+                </div>
               </div>
-              <button>
-                <NavLink to={"/configure-a-bike"} onClick={() => { window.scrollTo(0, 0) }}>
-                  RETURN TO SHOP
-                </NavLink>
-              </button>
-            </>
-            :''}
+            }
+          </div>
+          {items.length === undefined ? '' :
+            <div className="billing-adress-desktop">
+              <BillingAddress />
+            </div>
+          }
         </div>
+        {items.length === undefined ? '' :
+          <div className="billing-adress-mobile">
+            <BillingAddress />
+          </div>
+        }
       </section >
     </>
   )
